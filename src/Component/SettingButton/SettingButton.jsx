d543ce25cf9e949654b5133a98800455e2819165
Chamindu Nevandith <152043450+ChaminduNevandith@users.noreply.github.com>
2024-08-15 16:13:15 +0530

    Update and make it responsive SettingButton.jsx

--- src/Component/SettingButton/SettingButton.jsx
+++ src/Component/SettingButton/SettingButton.jsx
@@ -4,10 +4,10 @@ import SettingsIcon from '@mui/icons-material/Settings';
 const SettingButton = ({ setIsSidebarOpen }) => {
   return (
     <button
-      className="relative w-14 h-14 bg-purple-500 rounded-lg flex items-center justify-center hover:shadow-lg hover:bg-purple-600"
+      className="relative w-14 h-14 sm:w-11 sm:h-11 md:w-12 md:h-12 lg:w-13 lg:h-13 bg-purple-500 rounded-lg flex items-center justify-center hover:shadow-lg hover:bg-purple-600"
       onClick={() => setIsSidebarOpen(true)}
     >
-      <SettingsIcon className="text-white animate-spin-slow" />
+      <SettingsIcon className="text-white text-xs sm:text-sm md:text-base lg:text-lg animate-spin-slow" />
     </button>
   );
 };
