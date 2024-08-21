a6fd479bf8374a82f6cef43da5d9049b725137b2
ChaminduNevandith <cnewandith123@gmail.com>
2024-08-21 15:10:21 +0530

    added poppins font as the main font

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'], // Add Poppins as the default sans font
+      },
       animation: {
         'spin-slow': 'spin 4s linear infinite',
       },
