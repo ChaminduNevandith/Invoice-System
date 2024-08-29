d69726272aceb03244f21d34b697be3b0829d448
ChaminduNevandith <cnewandith123@gmail.com>
2024-08-29 16:08:04 +0530

    Added Supplier Credit Logs pages

---
+++ src/Pages/Suppliers/Supplier Credit Logs/SupplierCreditLogs.jsx
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+import { Search } from '@mui/icons-material'; // Example of MUI icons
+import { Pagination } from '@mui/material';
+
+
+function SupplierCreditLogs() {
+  return (
+    <div className="bg-gray-100 absolute top-[115px] right-0 w-full h-full overflow-auto p-4 sm:p-6 md:p-8 lg:p-10 animate-fadeIn  pb-[150px]">
+      <ListCustomers />
+    </div>
+  );
+}
+
+function ListCustomers() {
+
+  const [customers, setCustomers] = useState([
+    { id: 1, date: '03/04/2023', CreditType: 'Invoice', CreditAmount: '1000', DebitAmount: '2000',Total: '2000',PaymentType:'Full' , PaymentMethod: 'Cash' , Chequetype: '-', ChequeDate: '-', ChequeName: '-' },
+    { id: 2, date: '03/04/2023', CreditType: 'Payment', CreditAmount: '6500', DebitAmount: '0', Total: '2000',PaymentType:'Half' ,PaymentMethod: 'Online' , Chequetype: '-', ChequeDate: '-', ChequeName: '-'},
+    { id: 3, date: '03/04/2023', CreditType: 'Payment', CreditAmount: '3000', DebitAmount: '1000',Total: '2000',PaymentType:'Full' , PaymentMethod: '-', Chequetype: '-', ChequeDate: '-', ChequeName: '-' },
+    // Add more customers here...
+  ]);
+
+  const [page, setPage] = useState(1);
+
+  const handleChangePage = (event, value) => {
+    setPage(value);
+  };
+
+
+
+  return (
+    <div className="bg-white p-6 rounded-md shadow-md w-full ">
+      <h1 className=" text-2xl font-semibold text-[#264164] mb-1">Supplier Credit Logs</h1>
+      <hr className="bg-black mb-4"></hr>
+
+      <div className="flex flex-col md:flex-row justify-between mt-[20px] mb-[50px]">
+        {/* Search Bar */}
+        <div className="relative mb-4 md:mb-0">
+        <label className="block text-[#264164] mb-1 text-xs">Select Supplier</label>
+        <select className="p-2 border rounded-md w-full md:w-[250px] border-gray-300 focus:ring-2 focus:ring-blue-500 transition-all duration-200 focus:ring-purple-500 focus:ring-opacity-50 hover:ring-2 hover:ring-blue-300 hover:ring-opacity-50">
+            <option>Akila 724391</option>
+            <option>Nimal 236123</option>
+            <option>Banu 2242134</option>
+          </select>
+        </div>
+
+       {/* Sort Dropdown */}
+       <div className="relative ">
+          <div className='text-lg text-[#264164] font-semibold'> Total Credit Balance: LKR 291,000.00</div>
+          <div className='text-sm text-[#264164] font-light'>Akilla</div>
+          <div className='text-sm text-[#264164] font-light'>724391</div>
+        </div>
+      </div>
+
+      {/* Table */}
+      <div className="overflow-x-auto">
+        <table className="table-auto w-full border-collapse text-xs
+              xxl:text-sm
+              2xl:text-base
+              xl:text-sm
+              lg:text-xs
+              md:text-xs
+              sm:text-xs
+              xs:text-[10px]">
+          <thead>
+            <tr className="text-left text-gray-700 bg-[#F2F1F1]">
+            <th className="py-2 px-4 border-b text-[#264164]">Date</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Credit Type</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Credit Amount</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Debit Amount</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Total Credit</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Payment Type</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Payment Method</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Cheque type</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Cheque Date</th>
+              <th className="py-2 px-4 border-b text-[#264164]">Cheque Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {customers.map((customer) => (
+              <tr key={customer.id} className="table-row text-gray-700 text-left bg-[#FEF9FF]">
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Date">{customer.date}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Credit Type">{customer.CreditType}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Credit Amount">{customer.CreditAmount}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Debit Amount">{customer.DebitAmount}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Total Credit">{customer.Total}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Payment Type">{customer.PaymentType}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Payment Method">{customer.PaymentMethod}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Cheque type">{customer.Chequetype}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Cheque Name">{customer.ChequeName}</td>
+                <td className="py-2 px-4 border-b text-[#264164]" data-label="Cheque Date">{customer.ChequeDate}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+
+      {/* Pagination */}
+      <div className="flex justify-end mt-4">
+        <Pagination
+          count={3} // Replace with dynamic page count
+          page={page}
+          onChange={handleChangePage}
+          className="pagination-custom"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default SupplierCreditLogs;
