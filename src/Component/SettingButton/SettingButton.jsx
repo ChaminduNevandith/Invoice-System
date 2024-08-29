6f20ad6cdb09bc0eca193dbb8844862eda3f07cb
ChaminduNevandith <cnewandith123@gmail.com>
2024-08-29 08:46:07 +0530

    change sizes

--- src/Component/SettingButton/SettingButton.jsx
+++ src/Component/SettingButton/SettingButton.jsx
@@ -4,7 +4,7 @@ import SettingsIcon from '@mui/icons-material/Settings';
 const SettingButton = ({ setIsSidebarOpen }) => {
   return (
     <button
-      className="relative fixed bottom-4 right-0 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 2xl:w-16 2xl:h-16 bg-purple-500 rounded-lg flex items-center justify-center hover:shadow-lg hover:bg-purple-600"
+      className="relative fixed bottom-4 right-0 w-12 h-12 lg:w-14 lg:h-14 xl:w-14 xl:h-14 2xl:w-14 2xl:h-14 bg-purple-500 rounded-lg flex items-center justify-center hover:shadow-lg hover:bg-purple-600"
       onClick={() => setIsSidebarOpen(true)}
     >
       <SettingsIcon className="text-white text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 2xl:text-2xl animate-spin-slow" />
